5449483707c4bd9c82020d928c0405eb20b0aff5
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2021-02-11 15:46:33 -0500

    More JSON stuff and other minor revisions

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from nltk.corpus import stopwords
 from gensim.models import Word2Vec
 import spacy
 import json
+from datetime import datetime
 
 
 def get_url():
@@ -77,7 +78,7 @@ def parse_url(url):
     return words
 
 
-def get_top_nvj(text, top_w=12, pos=[], neg=[]):
+def get_top_nvj(text, top_w=12):
     """
     Get the top 'top_w' nouns, verbs, and adjectives related to the positive contributors and negative contributors.
 
@@ -94,36 +95,46 @@ def get_top_nvj(text, top_w=12, pos=[], neg=[]):
     top_verbs = []
     top_adj = []
 
-    for i in range(len(pos)):
-        try:
-            # Get 100 similar words using pos_terms and neg_terms
-            sim_words = word2vec.wv.most_similar(positive=pos, negative=neg, topn=100)
+    try:
+        # Get 100 similar words using pos_terms and neg_terms
+        sim_words = word2vec.wv.most_similar(positive=pos_terms, negative=neg_terms, topn=100)
 
-            # print("TERM={}:".format(pos_terms[i]))
-            for j in range(len(sim_words)):
-                # Get the POS tag from the current word from sim_words
-                pos = nltk.pos_tag(nltk.word_tokenize(sim_words[j][0]))
-                pos_tag = pos[0][1]
+        # print("TERM={}:".format(pos_terms[i]))
+        for j in range(len(sim_words)):
+            # Get the POS tag from the current word from sim_words
+            pos = nltk.pos_tag(nltk.word_tokenize(sim_words[j][0]))
+            pos_tag = pos[0][1]
 
-                # print("{} ({}), {}".format(sim_words[j][0], pos_tag, sim_words[j][1]))
+            # print("{} ({}), {}".format(sim_words[j][0], pos_tag, sim_words[j][1]))
 
-                # Get top results for each list.
-                if pos_tag in ('NN', 'NNP', 'NNS') and len(top_nouns) < top_w:
-                    top_nouns.append((sim_words[j][0], sim_words[j][1], pos_tag))
-                elif pos_tag in ('VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ') and len(top_verbs) < top_w:
-                    top_verbs.append((sim_words[j][0], sim_words[j][1], pos_tag))
-                elif pos_tag in ('JJ', 'JJR', 'JJS') and len(top_adj) < top_w:
-                    top_adj.append((sim_words[j][0], sim_words[j][1], pos_tag))
+            # Get top results for each list.
+            if pos_tag in ('NN', 'NNP', 'NNS') and len(top_nouns) < top_w:
+                top_nouns.append((sim_words[j][0], sim_words[j][1], pos_tag))
+            elif pos_tag in ('VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ') and len(top_verbs) < top_w:
+                top_verbs.append((sim_words[j][0], sim_words[j][1], pos_tag))
+            elif pos_tag in ('JJ', 'JJR', 'JJS') and len(top_adj) < top_w:
+                top_adj.append((sim_words[j][0], sim_words[j][1], pos_tag))
 
-        except KeyError as e:
-            print("ERROR! One or more keywords were not recognized as a vocabulary word! {}".format(e.args))
-            exit(1)
+    except KeyError as e:
+        print("ERROR! One or more keywords were not recognized as a vocabulary word! {}".format(e.args))
+        exit(1)
+
     return [top_nouns, top_verbs, top_adj]
 
 
-def write_to_json(filepath, data):
-    json_data = []
+def write_to_json(filepath, data, url):
+    """
+    Write related word results to JSON file.
+
+    :param filepath: JSON file path.
+    :param data: Data to parse into JSON file.
+    :param url: URL of the website being searched through.
+    """
+
+    # I'm bad at pretty printing.
+    json_data = [url, "POSITIVE WORDS: " + str(pos_terms), "NEGATIVE WORDS: " + str(neg_terms)]
     for i in range(len(data)):
+        json_data.append(("NOUNS:", "VERBS:", "ADJECTIVES:")[i])
         for j in data[i]:
             json_data.append([{"WORD": j[0], "SIMILARITY": j[1], "POS": j[2]}])
 
@@ -133,10 +144,11 @@ def write_to_json(filepath, data):
             print("Successfully wrote data to {}".format(filepath))
     except FileNotFoundError as e:
         print("ERROR! File not found at {}!".format(e.filename))
+    finally:
+        f.close()
 
 
 def main():
-    urls = get_url()
     for u in urls:
         try:
             print(u)
@@ -145,17 +157,18 @@ def main():
             print("HTTP Error {} while trying to open {}".format(e.code, e.url))
             continue
 
-        pos_terms = get_pos_terms()
-        neg_terms = get_neg_terms()
-
-        top_results = get_top_nvj(all_words, 12, pos_terms, neg_terms)
+        top_results = get_top_nvj(all_words, 12)
         print("\nTOP RESULTS FOR {}".format(u))
         print("NOUNS: {}\nVERBS: {}\nADJECTIVES: {}\n".format(top_results[0], top_results[1], top_results[2]))
 
-        path = "{}_results.json".format(str(pos_terms))
-        write_to_json(path, top_results)
+        path = "results_{}.json".format(datetime.now().strftime('%Y-%m-%w_%H-%M'))
+        write_to_json(path, top_results, u)
 
+
+urls = get_url()
+
+pos_terms = get_pos_terms()
+neg_terms = get_neg_terms()
 
 if __name__ == '__main__':
     main()
-
